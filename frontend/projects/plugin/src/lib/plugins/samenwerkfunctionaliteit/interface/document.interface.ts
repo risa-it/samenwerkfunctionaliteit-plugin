6396045d41afedb113643d678f73d764712195ea
Axel Plowman <axel@risa-it.nl>
2026-08-13 14:46:18 +0200

    Strictly type `documentId`

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/interface/document.interface.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/interface/document.interface.ts
@@ -1,8 +1,9 @@
 import { ConfidentialityType } from '../types/confidentiality.type';
+import { UUID } from '../types/uuid.type';
 
 export interface DocumentInterface {
   samenwerkingId: string;
-  documentId: string;
+  documentId: UUID;
   fileName: string;
   confidentialityLevel: ConfidentialityType;
   creationDate: string;
