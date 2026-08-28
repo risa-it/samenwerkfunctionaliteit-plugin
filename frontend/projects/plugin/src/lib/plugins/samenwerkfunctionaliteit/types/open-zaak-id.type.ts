6ae2ea14ec5d2cf44b9caaf219093b643973a5f9
furkang-risa <Furkan@risa-it.nl>
2026-08-28 13:53:35 +0200

    Update parameter type for toSamenwerking and toOpenZaakId, it might be undefined. Also update error message.

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/types/open-zaak-id.type.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/types/open-zaak-id.type.ts
@@ -2,9 +2,9 @@ export type OpenZaakId = string & {
   readonly __brand: unique symbol;
 };
 
-export function toOpenZaakId(value: string): OpenZaakId {
+export function toOpenZaakId(value: string | undefined): OpenZaakId {
   if (!isValidOpenZaakId(value)) {
-    throw new Error(`${value} is not a valid Open Zaak ID.`);
+    throw new Error(`${value} is not a valid Open Zaak ID, or doesn't exist.`);
   }
 
   return value as OpenZaakId;
--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/types/samenwerking-id.type.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/types/samenwerking-id.type.ts
@@ -2,9 +2,11 @@ export type SamenwerkingId = string & {
   readonly __brand: unique symbol;
 };
 
-export function toSamenwerkingId(value: string): SamenwerkingId {
+export function toSamenwerkingId(value: string | undefined): SamenwerkingId {
   if (!isValidSamenwerkingId(value)) {
-    throw new Error(`${value} is not a valid Samenwerking ID.`);
+    throw new Error(
+      `${value} is not a valid Samenwerking ID, or doesn't exist.`,
+    );
   }
 
   return value as SamenwerkingId;
